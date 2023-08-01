c0dae1e648a34aa98d43acf33391fac12edb0fc6
Martin Flasar <christian.flasar@gmail.com>
2023-08-01 16:58:16 +0200

    Added first setup of Client(VUE 3 + TS) and Server(JAVA SPRING BOOT + GRAPHQL + POSTGRESQL).

---
+++ server/src/main/java/com/eshop/wordun/WordunApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.wordun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordunApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/eshop/wordun/WordunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.wordun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
